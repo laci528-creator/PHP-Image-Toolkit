caa800bf45e1a3bd7330f6cd37ed750966d1b299
Laszlo Haraszti <laci528@gmail.com>
2026-08-20 15:49:20 +0200

    style: refine toolkit interface and active navigation

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,6 +1,9 @@
+<?php
+$currentPage = basename($_SERVER['PHP_SELF']);
+?>
 <nav class="main-nav">
-    <a href="index.php">Startseite</a>
-    <a href="resize.php">Skalieren</a>
-    <a href="convert.php">Format konvertieren</a>
-    <a href="watermark.php">Wasserzeichen</a>
+    <a href="index.php" class="<?= $currentPage === 'index.php' ? 'active' : '' ?>">Startseite</a>
+    <a href="resize.php" class="<?= $currentPage === 'resize.php' ? 'active' : '' ?>">Skalieren</a>
+    <a href="convert.php" class="<?= $currentPage === 'convert.php' ? 'active' : '' ?>">Format konvertieren</a>
+    <a href="watermark.php" class="<?= $currentPage === 'watermark.php' ? 'active' : '' ?>">Wasserzeichen</a>
 </nav>
